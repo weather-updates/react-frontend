dbac682866659e500bf9b07a59cbab910a4e3957
salmanmanekia <salmanmanekia@gmail.com>
2021-06-07 02:35:01 +0300

    Add the proper output path

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ let config =  {
     entry: path.resolve(__dirname, "src", "index.js"),
     output: {
         path: path.resolve(__dirname, "build"),
-        publicPath: '/'
+        publicPath: '/react-frontend/'
     },
     devServer: {
         historyApiFallback: true,
